282e1260f3c51f4391c04aa0ae14f953028e12a2
Edwin Ugwu <edwinbuike@gmail.com>
2024-06-07 05:15:09 +0100

    network module created

---
+++ README.md
@@ -0,0 +1,1 @@
+# Flyreserve Network Module
---
+++ main.tf
@@ -0,0 +1,176 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+locals {
+  vpc_name     = "${var.vpc_name}-${var.env_name}"
+  cluster_name = "${var.cluster_name}-${var.env_name}"
+}
+
+## AWS VPC definition
+resource "aws_vpc" "main" {
+  cidr_block           = var.main_vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "Name"                                        = local.vpc_name,
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared",
+  }
+}
+
+## Subnet definition
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "public-subnet-a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_a_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    "Name"                                        = "${local.vpc_name}-public-subnet-a"
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+}
+
+resource "aws_subnet" "public-subnet-b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_b_cidr
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    "Name"                                        = "${local.vpc_name}-public-subnet-b"
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+}
+
+resource "aws_subnet" "private-subnet-a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_a_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    "Name"                                        = "${local.vpc_name}-private-subnet-a"
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
+
+resource "aws_subnet" "private-subnet-b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_b_cidr
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    "Name"                                        = "${local.vpc_name}-private-subnet-b"
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
+
+## Internet gateway and routing tables for public subnets
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.vpc_name}-igw"
+  }
+}
+
+resource "aws_route_table" "public-route" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    "Name" = "${local.vpc_name}-public-route"
+  }
+}
+
+resource "aws_route_table_association" "public-a-association" {
+  subnet_id      = aws_subnet.public-subnet-a.id
+  route_table_id = aws_route_table.public-route.id
+}
+
+resource "aws_route_table_association" "public-b-association" {
+  subnet_id      = aws_subnet.public-subnet-b.id
+  route_table_id = aws_route_table.public-route.id
+}
+
+## NAT gateway and routing tables for private subnets
+resource "aws_eip" "nat-a" {
+  domain = "vpc"
+  tags = {
+    "Name" = "${local.vpc_name}-NAT-a"
+  }
+}
+
+resource "aws_eip" "nat-b" {
+  domain = "vpc"
+  tags = {
+    "Name" = "${local.vpc_name}-NAT-b"
+  }
+}
+
+resource "aws_nat_gateway" "nat-gw-a" {
+  allocation_id = aws_eip.nat-a.id
+  subnet_id     = aws_subnet.public-subnet-a.id
+  depends_on    = [aws_internet_gateway.igw]
+
+  tags = {
+    "Name" = "${local.vpc_name}-NAT-gw-a"
+  }
+}
+
+resource "aws_nat_gateway" "nat-gw-b" {
+  allocation_id = aws_eip.nat-b.id
+  subnet_id     = aws_subnet.public-subnet-b.id
+  depends_on    = [aws_internet_gateway.igw]
+
+  tags = {
+    "Name" = "${local.vpc_name}-NAT-gw-b"
+  }
+}
+
+resource "aws_route_table" "private-route-a" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat-gw-a.id
+  }
+
+  tags = {
+    "Name" = "${local.vpc_name}-private-route-a"
+  }
+}
+
+resource "aws_route_table" "private-route-b" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat-gw-b.id
+  }
+
+  tags = {
+    "Name" = "${local.vpc_name}-private-route-b"
+  }
+}
+
+resource "aws_route_table_association" "private-a-association" {
+  subnet_id      = aws_subnet.private-subnet-a.id
+  route_table_id = aws_route_table.private-route-a.id
+}
+
+resource "aws_route_table_association" "private-b-association" {
+  subnet_id      = aws_subnet.private-subnet-b.id
+  route_table_id = aws_route_table.private-route-b.id
+}
